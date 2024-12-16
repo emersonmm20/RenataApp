ab3f53277ee41c4531c485e441c25a53c41d0334
Emerson Gonzalez <emersonmm20@gmail.com>
2024-12-16 14:43:51 -0500

    inicios de Renata

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 import logo from './logo.svg';
+import {TODO,Statistics,Menu} from './components/tasks.js';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [menuActive,setMenuActive]= useState(false)
+
+  const showMenu=()=>{
+    if (menuActive){
+      document.getElementById("menu-container").style.display="flex"
+
+    }
+    else{
+      document.getElementById("menu-container").style.display="none"
+    }
+    setMenuActive(!menuActive)
+
+  }
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* ___________________Header_____________________ */}
+      <div className='header'>
+        <div className='header-left'>
+          <button onClick={showMenu} className='show-menu-button'><img src={require('./icons/menu.png')}/></button>
+          <h1>Renata</h1>
+        </div>
+        <div className='header-rigth'>
+          <ul>
+            <li className='header-option'><button>calculator</button></li>
+            <li className='header-option'><button>note</button></li>
+          </ul>
+        </div>
+      </div>
+      {/* ___________________MENU_____________________ */}
+      <div id='menu-container'>
+
+        <Menu/>
+        <button onClick={showMenu}><img src={require('./icons/close.png')} /></button>
+      </div>
+      {/* ___________________MENU_____________________ */}
+      <div className='view'>
+      <Statistics/>
+      <TODO />
+      </div>
+      
+      
     </div>
   );
 }
---
+++ src/components/tasks.js
@@ -0,0 +1,24 @@
+
+export const TODO = ()=>{
+    return (
+        <div className="todo-list">
+            <p>.</p>
+        </div>
+    );
+}
+export const Statistics = ()=>{
+    return (
+        <div className="stadistics">
+            <p>.</p>
+        </div>
+    )
+}
+export const Menu = (close)=>{
+
+
+    return (
+        <div id="menu">
+            <p>aaaaaaaaaaasldkjhasdljashd</p>
+        </div>
+    )
+}
